91e8a6f345e1b1e44bd9c695fb7a71768fb3a817
Martha Watson <marthacdenzer@gmail.com>
2024-11-14 17:46:19 -0700

    Update .gitignore and remove .env from tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-
 import App from './App.tsx';
 import CandidateSearch from './pages/CandidateSearch.tsx';
 import SavedCandidates from './pages/SavedCandidates.tsx';
 import ErrorPage from './pages/ErrorPage.tsx';
-
 const router = createBrowserRouter([
   {
     path: '/',
@@ -24,7 +22,6 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-
 const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
